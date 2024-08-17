855a16bdfa248ddbba10bb8b36b164b087c53570
Eduardo Bezerra <eduardo.escolar447@gmail.com>
2024-08-16 22:59:46 -0300

    Filtro arcade

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
+import { ARCADE_FILTER } from "./gfx/ArcadeFilter.mjs"
+
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
 let fps = 0
 let fps_temp = 0
 
+let arcade_filter = new ARCADE_FILTER(canvas.width, canvas.height)
+
 function Update(){
     console.log(`FPS: ${fps}`)
+
+    arcade_filter.update(fps)
 }
 
 function FpsCalc(){
@@ -19,6 +25,8 @@ function Render(){
     fps_temp++
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    arcade_filter.render(ctx)
 }
 
 function GameLoop(){
